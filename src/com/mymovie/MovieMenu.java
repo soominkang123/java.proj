a308f06a45480870bba17136228bf3d82b4605bd
soomin Kang <tnals3256@naver.com>
2023-12-04 16:37:57 +0900

    12월 4일 movie test 수정

--- src/com/mymovie/MovieMenu.java
+++ src/com/mymovie/MovieMenu.java
@@ -25,6 +25,7 @@ public class MovieMenu {
 			System.out.println("3. 영화 제목으로 출력");
 			System.out.println("4. 영화 감독명으로 출력");
 			System.out.println("5. 영화 장르명로 출력 ");
+			System.out.println("6. 영화 정보 삭제");
 			System.out.println("0. 종료");
 			System.out.println("============================");
 			System.out.println("위의 번호를 선택하세요>>>>");
@@ -32,29 +33,56 @@ public class MovieMenu {
 			
 			switch (select) {
 			case 1:
-				System.out.println("영화명을 입려하세요>>>");
+				System.out.println("영화명을 입력하세요>>>");
 				String title = sc.next(); 
-				System.out.println("감독명을 입려하세요>>>");
+				System.out.println("감독명을 입력하세요>>>");
 				String director = sc.next(); 
-				System.out.println("등급을 입려하세요>>>");
+				System.out.println("등급을 입력하세요>>>");
 				int grade = sc.nextInt(); 
-				System.out.println("장르을 입려하세요>>>");
+				System.out.println("장르를 입력하세요>>>");
 				String genre = sc.next(); 
 				
-				mg.add(new Movie(title, director, grade, genre));		
+				mg.add(new Movie(title, director, grade, genre));
+				System.out.println("성공적으로 추가되었습니다.");
+				System.out.println("============================");
 				break; 
 				
 			case 2:
+				System.out.println(" 추가된 영화의 정보입니다.");
 				mg.search();
+				System.out.println("======================");
 				break; 
+				
+				
 			case 3:
+				System.out.println("검색할 영화 명을 입력하세요:");
+				String sTitle = sc.next();
+				mg.search(sTitle);
+				System.out.println("=======================");
 				break; 
+				
 			case 4:
+				System.out.println("검색할 감독 명을 입력하세요: ");
+				String sDirector =sc.next();
+				mg.searchDirector(sDirector);
+				System.out.println("======================");
 				break; 
+			
 			case 5:
+				System.out.println("검색할 영화 장르를 입력하세요 :");
+				String sGenre = sc.next();
+				System.out.println("========================");
 				break; 
+				
 			case 6:
-				System.out.println("프로그램을 종료 합니다. ");
+				System.out.println("삭제할 영화 명을 입력하세요: ");
+				String dTitle = sc.next();
+				mg.delete(dTitle);
+				System.out.println("==================");
+				break;
+				
+			case 0:	
+				
 				break out; 
 			
 			}
@@ -62,6 +90,8 @@ public class MovieMenu {
 			
 			
 		}while (flag); 
+	System.out.println("프로그램을 종료 합니다. ");
+	sc.close();
 	}
 	
 	
